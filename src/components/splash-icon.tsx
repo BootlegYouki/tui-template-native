4953cfd593c129ca948e0d1ff189b2140ac163d5
Mark Darren <152751762+BootlegYouki@users.noreply.github.com>
2026-06-04 10:10:12 +0800

    feat: configure stable SDK 51 and add Universal Dev Client packages and workflows

--- src/components/splash-icon.tsx
+++ src/components/splash-icon.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Svg, { Path, SvgProps } from 'react-native-svg';
+import { SvgProps } from 'react-native-svg';
+import { IconSvg } from './icon-svg';
 
 interface SplashIconProps extends SvgProps {
   color?: string;
@@ -7,16 +8,11 @@ interface SplashIconProps extends SvgProps {
 }
 
 export const SplashIcon: React.FC<SplashIconProps> = ({
-  color = '#000000',
+  color,
   size = 120,
   ...props
 }) => {
   return (
-    <Svg viewBox="0 0 16 16" width={size} height={size} {...props}>
-      {/* Chevron: > */}
-      <Path d="M2 3 L8 8 L2 13 L4 13 L10 8 L4 3 Z" fill={color} />
-      {/* Underscore: _ */}
-      <Path d="M10 12 L15 12 L15 14 L10 14 Z" fill={color} />
-    </Svg>
+    <IconSvg color={color} width={size} height={size} {...props} />
   );
 };
